63d2339fa03adb7a7b9a495be5d73f43399a7d98
AymenDaoudi <aymendaoudi@outlook.com>
2015-04-05 00:46:19 +0100

    commit

--- Helpers/PropertyInfoComparer.cs
+++ Helpers/PropertyInfoComparer.cs
@@ -13,6 +13,8 @@ namespace ViewModelsMerger.Helpers
         {
             if (ReferenceEquals(x, y)) return true;
             if (ReferenceEquals(x, null) || ReferenceEquals(y, null)) return false;
+
+
             return x.Name == y.Name;
         }
 
